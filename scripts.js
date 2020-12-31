31f5dd07446dbf8a7c3b390d1c392d6996693073
Isaac Agyei <agyeis@hotmail.com>
2020-12-30 18:30:12 -0700

    skip button functioning

--- scripts.js
+++ scripts.js
@@ -3,8 +3,8 @@ const video = player.querySelector('.viewer')
 const progress = player.querySelector('.progress')
 const progressBar = player.querySelector('.progress__filled')
 const toggle = player.querySelector('.toggle')
-const skipButtons = player.querySelector('[data-skip]')
-const ranges = player.querySelector('.player__slider')
+const skipButtons = player.querySelectorAll('[data-skip]')
+const ranges = player.querySelectorAll('.player__slider')
 
 
 function togglePlay() {
@@ -17,8 +17,17 @@ function updateButton() {
   toggle.textContent = icon;
 }
 
+function skip() {
+
+  video.currentTime += parseFloat(this.dataset.skip)
+
+}
+
 video.addEventListener('click', togglePlay)
 video.addEventListener('play', updateButton)
 video.addEventListener('pause', updateButton)
 
 toggle.addEventListener('click', togglePlay)
+
+skipButtons.forEach(button => button.addEventListener('click', skip))
+
